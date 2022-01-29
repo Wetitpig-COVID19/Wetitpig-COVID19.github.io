3eb49eca5d979080d8c3ae8b6474439a44b567d3
Wetitpig <winsto003@hotmail.com>
2022-01-29 14:55:08 +0800

    Use xargs instead of zstdmt

--- build-scripts/compress.sh
+++ build-scripts/compress.sh
@@ -2,14 +2,17 @@
 
 set -x
 
+cpu_count=$(nproc || printf '2')
 zstFiles=$(ls -1 assets/maps/*.zst 2>/dev/null | wc -l)
 
 if [ $zstFiles = 0 ]; then
-	find assets/maps -name "*.json" -exec zstdmt -17 {} \;
+	find assets/maps -name "*.json" | \
+		xargs -P$cpu_count -I {} sh -c 'set -x; zstd -17 {};'
 fi
 
 if [ $(ls -1 assets/maps/*.json | wc -l) != $(wc -l < assets/data/checksum.txt) ] || [ $(sha256sum -c --status assets/data/checksum.txt; printf $?) != 0 ]; then
-	find assets/data -name "*.json" -exec zstdmt -17 {} \;
+	find assets/data -name "*.json" | \
+		xargs -P$cpu_count -I {} sh -c 'set -x; zstd -17 {};'
 fi
 
 rm assets/data/checksum.txt
