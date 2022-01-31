1f9249b128c33023ef0a90c8b1561385d515464f
Wetitpig <winsto003@hotmail.com>
2022-01-31 09:13:53 +0800

    Autoprefix CSS

--- build-scripts/minify.sh
+++ build-scripts/minify.sh
@@ -4,12 +4,19 @@ set -x
 
 cpu_count=$(nproc 2>/dev/null || printf '2')
 
-node_modules/html-minifier/cli.js --collapse-whitespace --use-short-doctype -o index.html index.html
+npx html-minifier --collapse-whitespace --use-short-doctype -o index.html index.html
 
 find assets -name "*.css" | \
 	xargs -P$cpu_count -I {} \
-	sh -c "set -x; node_modules/clean-css-cli/bin/cleancss -o {} {}"
+	sh -c "
+		set -x;
+		npx postcss {} --use autoprefixer --replace;
+		npx cleancss -o {} {};
+	"
 
 find assets -name "*.js" | \
 	xargs -P$cpu_count -I {} \
-	sh -c "set -x; node_modules/uglify-js/bin/uglifyjs {} -cmo {}"
+	sh -c "
+		set -x;
+		npx uglifyjs {} -cmo {};
+	"
