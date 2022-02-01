6db5b5f38bf605d28cbaaa4ce3519d1ca82bbed4
Wetitpig <winsto003@hotmail.com>
2022-02-01 21:50:03 +0800

    Add clean command

--- build-scripts/compress.sh
+++ build-scripts/compress.sh
@@ -6,12 +6,12 @@ cpu_count=$(nproc 2>/dev/null || printf '2')
 zstFiles=$(ls -1 assets/maps/*.zst 2>/dev/null | wc -l)
 
 if [ $zstFiles = 0 ]; then
-	find assets/maps -name "*.json" | \
+	find assets/maps -name '*.json' | \
 		xargs -P$cpu_count -I {} sh -vc 'zstd -17 {};'
 fi
 
 if [ $(ls -1 assets/maps/*.json | wc -l) != $(wc -l < assets/data/checksum.txt) ] || [ $(sha256sum -c --status assets/data/checksum.txt; printf $?) != 0 ]; then
-	find assets/data -name "*.json" | \
+	find assets/data -name '*.json' | \
 		xargs -P$cpu_count -I {} sh -vc 'zstd -17 -f {};'
 fi
 
